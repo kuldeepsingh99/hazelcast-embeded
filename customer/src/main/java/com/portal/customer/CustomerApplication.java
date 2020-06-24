9f83cffe9bab9ef8f95d9a3afa15fb1286c6f5f7
Kuldeep Singh <kuldeepsingh99@gmail.com>
2020-06-24 10:23:37 +0530

    enable CORS

--- customer/src/main/java/com/portal/customer/CustomerApplication.java
+++ customer/src/main/java/com/portal/customer/CustomerApplication.java
@@ -3,6 +3,9 @@ package com.portal.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +13,17 @@ public class CustomerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
